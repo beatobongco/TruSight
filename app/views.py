3c7dc57deacf8dd26797f7f8031aae428cf42d15
Beato Bongco <beatobongco@gmail.com>
2014-08-17 15:12:14 +0800

    Semifinal commit

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ import keen
 from app import app
 from flask import url_for, json, redirect, render_template, request
 
+mykeys = {}
+primary = ''
 keen.project_id = app.config['KEEN_PROJECT_ID']
 keen.write_key = app.config['KEEN_WRITE_KEY']
 keen.read_key = app.config['KEEN_READ_KEY']
@@ -29,7 +31,10 @@ def hello():
     if new_file:
       filename = new_file.filename
       new_file.save(os.path.join(uploads_directory, filename))
-      send_data_to_keen(filename)
+      global mykeys
+      global primary
+      primary, mykeys = send_data_to_keen(filename)
+      print mykeys, primary
     return(redirect(url_for('dashboard', dashboard_name=filename)))
   return render_template('index.html') 
 
@@ -46,12 +51,18 @@ def upload():
 
 @app.route("/dashboard/<dashboard_name>", methods=['GET'])
 def dashboard(dashboard_name):
-  return render_template('dashboard.html')
+  return render_template('dashboard.html', title=dashboard_name, keys = mykeys, event_collect = primary)
 
 def send_data_to_keen(keen_json):
+  hey = ""
   contents = read_contents(keen_json)
   for key in contents.keys():
+    print "XANDER LOOK HERE"
+    hey = key
     events = contents.get(key)
     for event in events:
       keen.add_events({str(key): [event]})
+      mynewkeys = event.keys()
+
+  return hey, mynewkeys
 
